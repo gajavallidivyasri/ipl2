ec2ab90f687fba1ad2ef61fd1f880ca039a9e45b
gajavallidivyasri <gajavallidivyasri@gmail.com>
2025-08-08 22:53:52 +0530

    main

---
+++ src/main/java/com/vignan/maven/ipl2/App.java
@@ -0,0 +1,13 @@
+package com.vignan.maven.ipl2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
