e049b0a9c339eb45b7b436d88418db55f87af7d3
gajavallidivyasri <gajavallidivyasri@gmail.com>
2025-08-08 23:24:52 +0530

    Update Main.java

--- src/main/java/com/vignan/maven/ipl2/Main.java
+++ src/main/java/com/vignan/maven/ipl2/Main.java
@@ -1,5 +1,30 @@
 package com.vignan.maven.ipl2;
 
-public class Main {
+package com.vignan.maven.ipl2;
 
+
+
+import controller.PlayerController;
+import model.Player;
+import view.PlayerView;
+
+public class Main {
+    public static void main(String[] args) {
+        PlayerView view = new PlayerView();
+        PlayerController controller = new PlayerController(view);
+
+        // Adding Players
+        controller.addPlayer(new Player(1, "Virat Kohli", "RCB", "Batsman", 7000, 4));
+        controller.addPlayer(new Player(2, "Jasprit Bumrah", "MI", "Bowler", 500, 150));
+
+        // Display all players
+        controller.displayAllPlayers();
+
+        // Update a player’s stats
+        controller.updateStats(1, 7100, 5);
+
+        // Display again after update
+        controller.displayAllPlayers();
+    }
 }
+
